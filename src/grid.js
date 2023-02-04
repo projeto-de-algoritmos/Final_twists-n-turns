30cc964537b60d18597b5f27f67c965b8f1e71d0
Guilherme Puida <guilhermepmoreira00@gmail.com>
2023-02-04 00:43:06 -0300

    Create Grid class

---
+++ src/grid.js
@@ -0,0 +1,82 @@
+import { range, randomFromArray } from './utils.js';
+import { Cell } from './cell.js';
+
+// Represents a grid.
+//
+// This contains cells and handles the relationships between cells.
+export class Grid {
+  constructor(rows, cols, cellSize) {
+    this.rows = rows;
+    this.cols = cols;
+    this.cellSize = cellSize;
+
+    // Initially, there are no cells in the grid.
+    this.grid = this.prepareGrid();
+    this.configureCells();
+  }
+
+  // Populate grid with cells in each spot.
+  prepareGrid() {
+    return range(this.rows).map(row =>
+      range(this.cols).map(col =>
+        new Cell(row, col)
+      )
+    );
+  }
+
+  // Links cells to their neighbors.
+  //
+  // This takes into account eges.
+  configureCells() {
+    this.cells.forEach(cell => {
+      const { row, col } = cell;
+      cell.north = this.at(row - 1, col);
+      cell.south = this.at(row + 1, col);
+      cell.west = this.at(row, col - 1);
+      cell.east = this.at(row, col + 1);
+    });
+  }
+
+  // Return cells as a one-dimensional array.
+  //
+  // This returns a new array and not a reference to the internal
+  // grid structure, so callers are able to mutate as needed without
+  // messing up the internal representation of the class.
+  get cells() {
+    return this.grid.flat();
+  }
+
+  get size() {
+    return this.rows * this.cols;
+  }
+
+  // Get a cell at (row, col).
+  at(row, col) {
+    if (row < 0 || row > this.rows - 1 ||
+      col < 0 || col > this.grid[row].length)
+      return undefined;
+
+    return this.grid[row][col];
+  }
+
+  // Get a random cell from the grid.
+  randomCell() {
+    return randomFromArray(this.cells);
+  }
+
+  // Creates a snapshot of the current state of the grid.
+  //
+  // This is useful for storing a history of changes during
+  // the execution of an algorithm, so the steps can be used
+  // for animation at a later time.
+  createSnapshot() {
+    return this.cells.map(cell => ({
+      row: cell.row,
+      col: cell.col,
+      north: !!cell.north,
+      west: !!cell.west,
+      east: cell.hasLink(cell.east),
+      south: cell.hasLink(cell.south),
+    }));
+  }
+}
